91d777ec65336bde8891f9d13ca4e8696aa2c050
soobin376 <s-o_obin@nate.com>
2013-07-15 19:50:20 +0900

    restify를 이용한 echoserver 테스트

--- restapiecho.js
+++ restapiecho.js
@@ -6,5 +6,5 @@ function send(req,res,next){
 var server=restify.createServer();
 server.get('/echo/:name',send);
 server.listen(9000,function(){
-	console.log('%s start ', server.name);
+	console.log('%s echo server start ', server.name);
 });
